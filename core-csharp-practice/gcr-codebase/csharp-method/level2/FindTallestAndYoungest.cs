54d90d737a10a56c14c0b15b9b845019a11d03f5
Anoopkumargithub <anoopkumar7818@outlook.com>
2025-12-26 00:13:31 +0530

    [Anoop Kumar] ADD: Implement FindTallestAndYoungest and NumberCheck classes for height and age comparisons

---
+++ core-csharp-practice/gcr-codebase/csharp-method/level2/FindTallestAndYoungest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.methods.level2
+{
+    internal class FindTallestAndYoungest
+    {
+        static int FindYoungest(int[] ages)
+        {
+            int youngestIndex = 0;
+
+            for (int i = 1; i < ages.Length; i++)
+            {
+                if (ages[i] < ages[youngestIndex])
+                {
+                    youngestIndex = i;
+                }
+            }
+            return youngestIndex;
+        }
+
+        static int FindTallest(int[] heights)
+        {
+            int tallestIndex = 0;
+
+            for (int i = 1; i < heights.Length; i++)
+            {
+                if (heights[i] > heights[tallestIndex])
+                {
+                    tallestIndex = i;
+                }
+            }
+            return tallestIndex;
+        }
+
+        static void Main()
+        {
+            int[] Heights = new int[3];
+            int[] Age = new int[3];
+
+            string[] Names = { "Amar", "Akbar", "Anthony" };
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Enter Height of {Names[i]} : ");
+                Heights[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Enter Age of {Names[i]} : ");
+                Age[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int Youngest = FindYoungest(Age);
+            int Tallest = FindTallest(Heights);
+
+
+            Console.WriteLine($"Youest Person in the group is  {Names[Youngest]} and Tallest is {Names[Tallest]}");
+        }
+    }
+
+}
---
+++ core-csharp-practice/gcr-codebase/csharp-method/level2/NumberCheck.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.methods.level2
+{
+    internal class NumberCheck
+    {
+        static bool IsPositive(int number)
+        {
+            return number >= 0;
+        }
+
+        static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        static int Compare(int number1, int number2)
+        {
+            if (number1 > number2)
+                return 1;
+            else if (number1 == number2)
+                return 0;
+            else
+                return -1;
+        }
+
+        static void Main()
+        {
+            int[] numbers = new int[5];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Enter a number:");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (IsPositive(numbers[i]))
+                {
+                    if (IsEven(numbers[i]))
+                    {
+                        Console.WriteLine($"{numbers[i]} is Positive and Even");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{numbers[i]} is Positive and Odd");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{numbers[i]} is Negative");
+                }
+            }
+            int result = Compare(numbers[0], numbers[numbers.Length - 1]);
+
+            if (result == 1)
+            {
+                Console.WriteLine("First element > Last element");
+            }
+            else if (result == 0)
+            {
+                Console.WriteLine("First element = Last element");
+            }
+            else
+            {
+                Console.WriteLine("First element < Last element");
+            }
+        }
+    }
+}
