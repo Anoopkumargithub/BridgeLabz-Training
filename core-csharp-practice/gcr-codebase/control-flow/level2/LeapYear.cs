eb9977b9bef9cacd4bf610a55c68b53258c824bc
Anoopkumargithub <anoopkumar7818@outlook.com>
2025-12-25 22:50:37 +0530

    [Anoop Kumar] FIX: Correct variable name in LeapYear calculation logic

--- core-csharp-practice/gcr-codebase/control-flow/level2/LeapYear.cs
+++ core-csharp-practice/gcr-codebase/control-flow/level2/LeapYear.cs
@@ -4,10 +4,10 @@ class LeapYear{
 	static void Main(){
 	Console.WriteLine("Enter Year: ");
 	int Year = Convert.ToInt32(Console.ReadLine());
-	if(LeapYear < 1582){
+	if(Year < 1582){
 		Console.WriteLine("Enter Year greater than 1582");
 	}else{
-		if((LeapYear % 4 == 0 && LeapYear % 100 != 0) || LeapYear % 400 == 0){
+		if((Year % 4 == 0 && Year % 100 != 0) || 	Year % 400 == 0){
 			Console.WriteLine("LeapYear");
 		}else Console.WriteLine("Not a LeapYear");
 	}
