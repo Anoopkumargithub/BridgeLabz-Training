4360955664f0ef7e999a2a4e55e1de8d6e86d00f
Anoopkumargithub <anoopkumar7818@outlook.com>
2026-01-04 18:39:56 +0530

    [Anoop Kumar] ADD: Implement Device and Thermostat classes with display functionality in SmartHomeSystem

---
+++ oops-charp-practice/gcr-code-base/csharp-inheritance/single-inheritance/SmartHomeSystem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.csharp_inheritance.single_inheritance
+{
+    // Superclass
+    internal class Device
+    {
+        protected int DeviceId;
+        protected string Status;
+
+        public Device(int DeviceId, string Status)
+        {
+            this.DeviceId = DeviceId;
+            this.Status = Status;
+        }
+
+        public void DisplayStatus()
+        {
+            Console.WriteLine("Device ID : " + DeviceId);
+            Console.WriteLine("Status    : " + Status);
+        }
+    }
+
+    // Subclass (Single Inheritance)
+    internal class Thermostat : Device
+    {
+        private int TemperatureSetting;
+
+        public Thermostat(int DeviceId, string Status, int TemperatureSetting)
+            : base(DeviceId, Status)
+        {
+            this.TemperatureSetting = TemperatureSetting;
+        }
+
+        public void DisplayStatus()
+        {
+            base.DisplayStatus();
+            Console.WriteLine("Temperature Setting : " + TemperatureSetting + "C");
+        }
+    }
+
+    class SmartHomeSystem
+    {
+        public static void Main(string[] args)
+        {
+            Thermostat t1 = new Thermostat(101, "ON", 24);
+
+            Console.WriteLine("Smart Device Status:");
+            t1.DisplayStatus();
+        }
+    }
+}
