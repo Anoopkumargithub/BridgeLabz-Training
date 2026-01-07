27da870605972c807765c3149c3a06f0e4eff05a
Anoopkumargithub <anoopkumar7818@outlook.com>
2026-01-07 10:33:20 +0530

    [Anoop Kumar] ADD: Implement employee wage computation system with Employee, EmployeeMenu, EmployeeUtilityImpl, and IEmployee classes and UC1 - Check Employee Attendance

---
+++ oops-charp-practice/scenario-based/employee-wage-computation-problem/IEmployee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.senariobased.employee_wage_computation_problem
+{
+    internal interface IEmployee
+    {
+        // abstract Method
+        Employee AddEmployee();
+
+        // UC1 - Check Employee Attendance
+        bool CheckEmployeeAttendance();
+
+
+    }
+}
