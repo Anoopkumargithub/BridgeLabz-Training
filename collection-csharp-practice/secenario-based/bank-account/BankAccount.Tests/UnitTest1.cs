c35fb8799ad3a28845dafdf07ad34442e12e12da
Anoopkumargithub <anoopkumar7818@outlook.com>
2026-01-31 11:15:34 +0530

    [Anoop Kumar] ADD: Implement BankAccount class with deposit and withdraw methods, and create unit tests for functionality

---
+++ collection-csharp-practice/secenario-based/bank-account/BankAccount.Tests/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using BankAccount;
+namespace BankAccount.Tests;
+
+public class Tests
+{
+    private BankAccount _account;
+
+    [SetUp]
+    public  void Setup()
+    {
+        _account = new BankAccount(1000);
+    }
+
+    [Test]
+    public void Test_Deposit_ValidAmount()
+    {
+        _account.Deposit(100);
+        Assert.That(_account.Balance, Is.EqualTo(1100));
+    }
+
+    [Test]
+    public void Test_Deposit_NegativeAmount()
+    {
+        Assert.Throws<ArgumentException>(() => _account.Deposit(-50));
+    }
+
+    [Test]
+    public void Test_Withdraw_ValidAmount()
+    {
+        _account.Deposit(200);
+        _account.Withdraw(50);
+        Assert.That(_account.Balance, Is.EqualTo(1150));
+    }
+
+    [Test]
+    public void Test_Withdraw_InsufficientFunds()
+    {
+        _account.Deposit(100);
+        Assert.Throws<InvalidOperationException>(() => _account.Withdraw(1250));
+    }
+}       
---
+++ collection-csharp-practice/secenario-based/bank-account/BankAccount/BankAccount.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace BankAccount;
+
+public class BankAccount
+{
+    public decimal Balance { get; set; }
+
+    public BankAccount(decimal initialBalance)
+    {
+        Balance = initialBalance;
+    }
+
+    // deposite method
+    public void Deposit(decimal amount)
+    {
+        if(amount < 0)
+        {
+            throw new ArgumentException("Deposit amount cannot be negative");
+        }
+        Balance += amount;
+    }
+
+    // withdraw method
+
+    public void Withdraw(decimal amount)
+    {
+        if(amount > Balance)
+        {
+            throw new InvalidOperationException("Insufficient funds.");
+        }
+
+        Balance -= amount;
+    }
+
+
+    // Main Mehthod
+
+    public static void Main(String[] args)
+    {
+        BankAccount account =  new BankAccount(1000);
+        Console.WriteLine("Initial Balance: " + account.Balance);
+
+        // Deposit 500
+        Console.WriteLine("Depositing 500...");
+        account.Deposit(500);
+
+        // Withdraw 200
+        Console.WriteLine("Withdrawing 200...");
+        account.Withdraw(200);
+
+        Console.WriteLine("Final Balance: " + account.Balance);
+    }
+
+}
